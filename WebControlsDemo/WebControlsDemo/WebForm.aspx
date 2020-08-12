5572ed222af29cf200e674881ed206342cf0a16c
komal rana <komal.rana@zensar.com>
2020-08-12 23:19:33 +0530

    Add assignment controls

---
+++ WebControlsDemo/WebControlsDemo/WebForm.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebControlsDemo.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="List Box"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged1" style="height: 27px">
+            </asp:RadioButtonList>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Radio Button List"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="CheckBoxList1_SelectedIndexChanged">
+            </asp:CheckBoxList>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Check Box List"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebControlsDemo/WebControlsDemo/WebForm.aspx.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebControlsDemo
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+
+
+                List<string> values = new List<string>();
+                values.AddRange(new String[] { "Item1", "Item2", "Item3", "Item4", "Item5" });
+                foreach (var n in values)
+                {
+                    ListBox1.Items.Add(n);
+                    RadioButtonList1.Items.Add(n);
+                    CheckBoxList1.Items.Add(n);
+
+                }
+
+            }
+
+            
+            }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ListBox1.SelectedIndex > -1)
+                Label1.Text = "You chose from ListBox : " + ListBox1.SelectedItem.Text;
+         
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            if (RadioButtonList1.SelectedIndex > -1)
+                Label2.Text = "You chose fom Radio Button : " + RadioButtonList1.SelectedItem.Text;
+
+        }
+
+        protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+                Label3.Text = "Selected Item(s):<br />";
+                
+                for (int i = 0; i < CheckBoxList1.Items.Count; i++)
+                {
+                    if (CheckBoxList1.Items[i].Selected)
+                    { Label3.Text += CheckBoxList1.Items[i].Text + "<br />"; }
+                }
+
+            
+        }
+    }
+}
